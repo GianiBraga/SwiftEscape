903269ee62f53f3ab08e9ccede5614e65e910e52
Giani Augusto Braga <giani.braga80@gmail.com>
2025-04-14 09:35:02 -0300

    Jogo finalizado

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
 from pygame.locals import (
+    RLEACCEL,
     K_UP,
     K_DOWN,
     K_LEFT,
@@ -25,8 +26,9 @@ SCREEN_HEIGHT = 600
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
-        self.surf.fill((255, 255, 255))
+        self.surf = pygame.image.load("jet.png").convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (60, 30))
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect()
 
     # Move the sprite based on user keypresses
@@ -55,8 +57,9 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
-        self.surf = pygame.Surface((20, 10))
-        self.surf.fill((255, 255, 255))
+        self.surf = pygame.image.load("missel.png").convert()
+        self.surf = pygame.transform.scale(self.surf, (50, 20)) 
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect(
             center=(
                 random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
@@ -72,7 +75,30 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+# Define the cloud object by extending pygame.sprite.Sprite
+# Use an image for a better-looking sprite
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("cloud.png").convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (70, 50)) 
+        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+        # The starting position is randomly generated
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
 
+    # Move the cloud based on a constant speed
+    # Remove the cloud when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-5, 0)
+        if self.rect.right < 0:
+            self.kill()
+            
+            
 # Initialize pygame
 pygame.init()
 
@@ -84,18 +110,27 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 250)
 
+# Create custom events for adding a new enemy and a cloud
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
+
 # Instantiate player. Right now, this is just a rectangle.
 player = Player()
 
 # Create groups to hold enemy sprites and all sprites
 # - enemies is used for collision detection and position updates
+# - clouds is used for position updates
 # - all_sprites is used for rendering
 enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
 # Variable to keep the main loop running
 running = True
+
+# Setup the clock for a decent framerate
+clock = pygame.time.Clock()
 
 # Main loop
 while running:
@@ -116,6 +151,13 @@ while running:
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
+        
+         # Add a new cloud?
+        elif event.type == ADDCLOUD:
+            # Create the new cloud and add it to sprite groups
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
             
     # Get all the keys currently pressed
     pressed_keys = pygame.key.get_pressed()
@@ -123,11 +165,12 @@ while running:
     # Update the player sprite based on user keypresses
     player.update(pressed_keys)
     
-    # Update enemy position
+    # Update enemy position and clouds
     enemies.update()
+    clouds.update()
 
-    # Fill the screen with black
-    screen.fill((0, 0, 0))
+    # Fill the screen with sky blue
+    screen.fill((135, 206, 250))
     
     # Draw all sprites
     for entity in all_sprites:
@@ -139,5 +182,11 @@ while running:
         player.kill()
         running = False
 
+    # Flip everything to the display
+    pygame.display.flip()
+
+    # Ensure program maintains a rate of 30 frames per second
+    clock.tick(30)
+    
     # Update the display
     pygame.display.flip()
